93e1b2ab33d6ac0c48a6507e6f08ba8a8caffc3e
Andrey Marakulin <annndruha.github@gmail.com>
2023-10-02 08:00:01 +0500

    Rename block

--- assets/js/language.js
+++ assets/js/language.js
@@ -26,13 +26,13 @@ function set_main_langugage(){
     $("#fullname").css("display", "block")
     $("#info").css("display", "block")
     $("#content").css("display", "block")
-    $("#contacts").css("display", "block")
+    $("#bt_contacts").css("display", "block")
     $("#fullname").css("display", "block")
 
     $("#second_fullname").css("display", "none")
     $("#second_info").css("display", "none")
     $("#second_content").css("display", "none")
-    $("#second_contacts").css("display", "none")
+    $("#bt_second_contacts").css("display", "none")
     $("#second_fullname").css("display", "none")
 
     document.title = $('meta[name="fullname"]').attr("content")
@@ -42,13 +42,13 @@ function set_second_langugage(){
     $("#fullname").css("display", "none")
     $("#info").css("display", "none")
     $("#content").css("display", "none")
-    $("#contacts").css("display", "none")
+    $("#bt_contacts").css("display", "none")
     $("#fullname").css("display", "none")
 
     $("#second_fullname").css("display", "block")
     $("#second_info").css("display", "block")
     $("#second_content").css("display", "block")
-    $("#second_contacts").css("display", "block")
+    $("#bt_second_contacts").css("display", "block")
     $("#second_fullname").css("display", "block")
     
     document.title = $('meta[name="second_fullname"]').attr("content")
